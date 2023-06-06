03109ab9f93f9658d5739306bc760dab1feaf043
Terry-Rossi <terryrossi1@gmail.com>
2023-06-06 08:03:23 -0700

    rename jsx files
    (Forgot extensions when created the files)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from '../movie-card/movie-card.jsx';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
