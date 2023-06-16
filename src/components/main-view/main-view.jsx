c5bef136e349da082338d4a0647f66118d443d99
Terry-Rossi <terryrossi1@gmail.com>
2023-06-16 16:33:32 -0700

    Update main-view.jsx
    Comments clean-up

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,38 +3,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-	const [movies, setMovies] = useState([
-		// {
-		// 	id: 1,
-		// 	title: 'Superman',
-		// 	image: 'https://m.media-amazon.com/images/I/510CJKGT6nL.jpg',
-		// 	actor: 'Henry Cavill',
-		// },
-		// {
-		// 	id: 2,
-		// 	title: 'Batman',
-		// 	image: 'https://m.media-amazon.com/images/I/71vUV3wgwKL._SL1500_.jpg',
-		// 	actor: 'Christian Bale',
-		// },
-		// {
-		// 	id: 3,
-		// 	title: 'Ironman',
-		// 	image: 'https://m.media-amazon.com/images/I/41W6U03Kr4L.jpg',
-		// 	actor: 'Robert Downey Jr',
-		// },
-		// {
-		// 	id: 4,
-		// 	title: 'Captain Amaerica',
-		// 	image: 'https://m.media-amazon.com/images/I/51-9t+DjrKL.jpg',
-		// 	actor: 'Chris Evans',
-		// },
-		// {
-		// 	id: 5,
-		// 	title: 'Wonder Woman',
-		// 	image: 'https://m.media-amazon.com/images/I/91Rtt7Zh7wL._SL1500_.jpg',
-		// 	actor: 'Gal Gabot',
-		// },
-	]);
+	const [movies, setMovies] = useState([]);
 
 	const [selectedMovie, setSelectedMovie] = useState(null);
 
